1c8a0c413eedbd6ca4aade3d8e97ced1fc4ae5da
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-02 16:16:37 +0900

    Checkpoint; working on router class

---
+++ src/main/java/Router.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Router {
+    Scanner scanner = new Scanner(System.in);
+    Controller controller = new Controller();
+    private Boolean running = true;
+
+    public void run(){
+        System.out.println("welcome to the Cookbook");
+
+        while (running){
+            displayTasks();
+            int userChoice = scanner.nextInt();
+            routeTask(userChoice);
+        }
+
+    }
+
+    public Boolean getRunning(){
+        return running;
+    }
+
+    public void setRunning(Boolean status){
+        this.running = status;
+    }
+
+    public void displayTasks(){
+        System.out.println("What do you want to do next?");
+        System.out.println(
+                "1. List all recipes\n" +
+                "2. Add a recipe\n" +
+                "3. Remove a recipe\n" +
+                "4. Exit"
+        );
+    }
+
+    public void routeTask(int userChoice){
+        switch (userChoice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                stop();
+                break;
+            default:
+                System.out.println("Invalid option, please try again");
+                break;
+        }
+    }
+
+    public void stop(){
+        setRunning(false);
+    }
+}
